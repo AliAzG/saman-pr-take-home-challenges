c8e043b73c3c22d4a78c5f389adc46cdabea76bc
Ali AzG <ali.azgohari@gmail.com>
2020-07-05 14:46:00 +0430

    same steps for directory 2017-12-20 done

--- html_parse.py
+++ html_parse.py
@@ -13,7 +13,6 @@ def data_extractor_step1():
             soup = BeautifulSoup(html_file, features="lxml")
         title = soup.find('title').get_text()
         artist_split = title.strip().split(":")[0]
-        print(30*"#", title.strip().split(":")[1])
         artist_list.append(artist_split)
     return json.dumps(artist_list, ensure_ascii=False)
 #print(data_extractor_step1())
@@ -72,3 +71,68 @@ def data_extractor_step4():
     return json.dumps(artist_list, ensure_ascii=False)
 
 #print(data_extractor_step4())
+
+#**Write a script that parses the HTML files in that directory, extracts the artist names and outputs a JSON array of the unique names to stdout.**
+
+def data_extractor_step5():
+    artist_list = []
+    dir_path = r"./lot-parser/data/2017-12-20"
+    for file_name in glob.glob(os.path.join(dir_path, "*.html")):
+        with open(file_name) as html_file:
+            soup = BeautifulSoup(html_file, features="lxml")
+        title = soup.find('title').get_text()
+        artist_split = title.strip().split(":")[0]
+        artist_list.append(artist_split)
+    return json.dumps(artist_list, ensure_ascii=False)
+#print(data_extractor_step5())
+
+#**Modify your script to extract the title of the work as well. Modify your output format to be an array of objects.**
+def data_extractor_step6():
+    artist_list = []
+    dir_path = r"./lot-parser/data/2017-12-20"
+    for file_name in glob.glob(os.path.join(dir_path, "*.html")):
+        with open(file_name) as html_file:
+            soup = BeautifulSoup(html_file, features="lxml")
+        title = soup.find('title').get_text()
+        artist_split, works = title.strip().split(":", 1)
+        art_work_dict = {"artist": artist_split, "works": [works.strip()]}
+        artist_list.append(art_work_dict)
+    return json.dumps(artist_list, ensure_ascii=False)
+#print(data_extractor_step6())
+
+#**Modify your script yet again, extracting the price realized and including it alongside the works.**
+def data_extractor_step7():
+    artist_list = []
+    dir_path = r"./lot-parser/data/2017-12-20"
+    for file_name in glob.glob(os.path.join(dir_path, "*.html")):
+        with open(file_name) as html_file:
+            soup = BeautifulSoup(html_file, features="lxml")
+        title = soup.find('title').get_text()
+        artist_split, works = title.strip().split(":", 1)
+        all_divs = soup.find_all('div')
+        price = all_divs[1].text
+        #works = title.split(":")[1].strip()
+        art_work_dict = {"artist": artist_split, "works": [{"title": works.strip(), "price": price}]}
+        #print(art_work_dict)
+        artist_list.append(art_work_dict)
+    return json.dumps(artist_list, ensure_ascii=False)
+#print(data_extractor_step7())
+
+#**Modify your script one more time, separating the currency from the amount.**
+
+def data_extractor_step8():
+    artist_list = []
+    dir_path = r"./lot-parser/data/2017-12-20"
+    for file_name in glob.glob(os.path.join(dir_path, "*.html")):
+        with open(file_name) as html_file:
+            soup = BeautifulSoup(html_file, features="lxml")
+        title = soup.find('title').get_text()
+        artist_split, works = title.strip().split(":", 1)
+        all_divs = soup.find_all('span')
+        amount = all_divs[1].text
+        currency = soup.find('span', {'class': 'currency'}).get_text()
+        art_work_dict = {"artist": artist_split, "works": [{"title": works.strip(), "currency": currency, "amount": amount}]}
+        artist_list.append(art_work_dict)
+    return json.dumps(artist_list, ensure_ascii=False)
+
+print(data_extractor_step8())
