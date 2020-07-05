5233bdd59a0053d1a55d3f98dfaff7a8fd948951
Ali AzG <ali.azgohari@gmail.com>
2020-07-05 15:13:39 +0430

    some correctiosn

--- html_parse.py
+++ html_parse.py
@@ -72,7 +72,7 @@ def data_extractor_step4():
 
 #print(data_extractor_step4())
 
-#**Write a script that parses the HTML files in that directory, extracts the artist names and outputs a JSON array of the unique names to stdout.**
+#**Write a script that parses the HTML files in that directory, extracts the artist names and outputs a JSON array of the unique names to stdout for 2017-12-20 directory.**
 
 def data_extractor_step5():
     artist_list = []
@@ -86,7 +86,7 @@ def data_extractor_step5():
     return json.dumps(artist_list, ensure_ascii=False)
 #print(data_extractor_step5())
 
-#**Modify your script to extract the title of the work as well. Modify your output format to be an array of objects.**
+#**Modify your script to extract the title of the work as well. Modify your output format to be an array of objects for 2017-12-20 directory.**
 def data_extractor_step6():
     artist_list = []
     dir_path = r"./lot-parser/data/2017-12-20"
@@ -100,7 +100,7 @@ def data_extractor_step6():
     return json.dumps(artist_list, ensure_ascii=False)
 #print(data_extractor_step6())
 
-#**Modify your script yet again, extracting the price realized and including it alongside the works.**
+#**Modify your script yet again, extracting the price realized and including it alongside the works for 2017-12-20 directory.**
 def data_extractor_step7():
     artist_list = []
     dir_path = r"./lot-parser/data/2017-12-20"
@@ -118,7 +118,7 @@ def data_extractor_step7():
     return json.dumps(artist_list, ensure_ascii=False)
 #print(data_extractor_step7())
 
-#**Modify your script one more time, separating the currency from the amount.**
+#**Modify your script one more time, separating the currency from the amount for 2017-12-20 directory.**
 
 def data_extractor_step8():
     artist_list = []
@@ -135,4 +135,19 @@ def data_extractor_step8():
         artist_list.append(art_work_dict)
     return json.dumps(artist_list, ensure_ascii=False)
 
-print(data_extractor_step8())
+#print(data_extractor_step8())
+
+def data_extractor_step9():
+    artist_list = []
+    dir_path = r"./lot-parser/data/2017-12-20"
+    for file_name in glob.glob(os.path.join(dir_path, "*.html")):
+        with open(file_name) as html_file:
+            soup = BeautifulSoup(html_file, features="lxml")
+        title = soup.find('title').get_text()
+        artist_split, works = title.strip().split(":", 1)
+        all_divs = soup.find_all('span')
+        amount = all_divs[1].text
+        currency = soup.find('span', {'class': 'currency'}).get_text()
+        art_work_dict = {"artist": artist_split, "works": [{"title": works.strip(), "currency": currency, "amount": amount}]}
+        artist_list.append(art_work_dict)
+    return json.dumps(artist_list, ensure_ascii=False)
