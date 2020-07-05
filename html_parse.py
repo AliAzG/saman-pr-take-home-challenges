99efdbc2711ac8d12340ab8cc989ae78b3c8c01a
Ali AzG <ali.azgohari@gmail.com>
2020-07-05 13:02:02 +0430

    step 4 done

--- html_parse.py
+++ html_parse.py
@@ -18,6 +18,7 @@ def data_extractor_step1():
     return json.dumps(artist_list, ensure_ascii=False)
 #print(data_extractor_step1())
 
+#**Modify your script to extract the title of the work as well. Modify your output format to be an array of objects.**
 def data_extractor_step2():
     artist_list = []
     dir_path = r"./lot-parser/data/2015-03-18"
@@ -31,3 +32,21 @@ def data_extractor_step2():
         artist_list.append(art_work_dict)
     return json.dumps(artist_list, ensure_ascii=False)
 #print(data_extractor_step2())
+
+#**Modify your script yet again, extracting the price realized and including it alongside the works.**
+def data_extractor_step3():
+    artist_list = []
+    dir_path = r"./lot-parser/data/2015-03-18"
+    for file_name in glob.glob(os.path.join(dir_path, "*.html")):
+        with open(file_name) as html_file:
+            soup = BeautifulSoup(html_file, features="lxml")
+        title = soup.find('title').get_text()
+        artist_split, works = title.strip().split(":", 1)
+        all_divs = soup.find_all('div')
+        price = all_divs[1].text
+        #works = title.split(":")[1].strip()
+        art_work_dict = {"artist": artist_split, "works": [{"title": works.strip(), "price": price}]}
+        #print(art_work_dict)
+        artist_list.append(art_work_dict)
+    return json.dumps(artist_list, ensure_ascii=False)
+print(data_extractor_step3())
